0c7e4015cc7f6cc511ae20154e7db3954942b016
ayeshaexe <ayeshaafarhan56@gmail.com>
2026-02-06 01:57:15 +0500

    changes

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -4,7 +4,7 @@ from typing import Optional
 class Settings(BaseSettings):
     NEON_DB_URL: str = "postgresql://neondb_owner:npg_69nHjwNzXBbd@ep-long-salad-a73u3ofv.ap-southeast-2.aws.neon.tech/neondb?sslmode=require"  # Default to SQLite for testing
     BETTER_AUTH_SECRET: str = "8ApAiAkyiQZg3XVId7GKjNyYXoFUw9vz"  # Default test secret
-    BETTER_AUTH_URL: str = "http://localhost:3000"
+    BETTER_AUTH_URL: str = "https://todo-part2.vercel.app"
 
     class Config:
         env_file = ".env"
